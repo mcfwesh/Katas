24719fb596b3a25113b04c119b2016145f461e9d
mcfwesh@gmail.com <mcfwesh@gmail.com>
2020-06-13 00:08:25 +0200

    challenge done

---
+++ 6 kyu/String subpattern recognition I.js
@@ -0,0 +1,21 @@
+//CHALLENGE
+/* In this kata you need to build a function to return either true/True or false/False if a string can be seen as the repetition of a simpler/shorter subpattern or not.
+
+For example:
+
+hasSubpattern("a") === false; //no repeated pattern
+hasSubpattern("aaaa") === true; //created repeating "a"
+hasSubpattern("abcd") === false; //no repeated pattern
+hasSubpattern("abababab") === true; //created repeating "ab"
+hasSubpattern("ababababa") === false; //cannot be entirely reproduced repeating a pattern
+Strings will never be empty and can be composed of any character (just consider upper- and lowercase letters as different entities) and can be pretty long (keep an eye on performances!). */
+
+//SOLUTION
+function hasSubpattern(string) {
+  for (let i = 0; i < string.length / 2; i++) {
+    let sliced = string.slice(0, i + 1);
+    let repeatSliced = sliced.repeat(string.length / (i + 1));
+    if (repeatSliced === string) return true;
+  }
+  return false;
+}
