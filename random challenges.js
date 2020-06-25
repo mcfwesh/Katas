88a606566733b6e9f2961e18dcb553348584d96c
mcfwesh@gmail.com <mcfwesh@gmail.com>
2020-06-26 00:28:39 +0200

    challenge completed

--- random challenges.js
+++ random challenges.js
@@ -133,3 +133,97 @@ truthCheck(
   ],
   "sex"
 );
+
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+// cid is a 2D array listing available currency.
+
+// The checkCashRegister() function should always return an object with a status key and a change key.
+
+// Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+// Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+// Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+// Currency Unit	Amount
+// Penny	$0.01 (PENNY)
+// Nickel	$0.05 (NICKEL)
+// Dime	$0.1 (DIME)
+// Quarter	$0.25 (QUARTER)
+// Dollar	$1 (ONE)
+// Five Dollars	$5 (FIVE)
+// Ten Dollars	$10 (TEN)
+// Twenty Dollars	$20 (TWENTY)
+// One-hundred Dollars	$100 (ONE HUNDRED)
+
+function checkCashRegister(price, cash, cid) {
+  const USNotes = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  };
+  let totalCash = cid.reduce((a, val, i) => a + val[1], 0);
+  let change = Math.round((cash - price) * 100) / 100;
+
+  let cidObject = Object.fromEntries(cid);
+  let result = {
+    status: "OPEN",
+    change: {
+      PENNY: 0,
+      NICKEL: 0,
+      DIME: 0,
+      QUARTER: 0,
+      ONE: 0,
+      FIVE: 0,
+      TEN: 0,
+      TWENTY: 0,
+      "ONE HUNDRED": 0,
+    },
+  };
+
+  if (totalCash < change) return { status: "INSUFFICIENT_FUNDS", change: [] };
+  if (totalCash === change) return { status: "CLOSED", change: cid };
+
+  for (let i = cid.length - 1; i >= 0; i--) {
+    const cashPerDenomination = cid[i][1];
+    const denominationLabel = USNotes[cid[i][0]];
+    let numberOfNotes = cashPerDenomination / denominationLabel;
+    while (change >= denominationLabel && numberOfNotes) {
+      result.change[cid[i][0]] = result.change[cid[i][0]] + denominationLabel;
+      numberOfNotes--;
+      change = Math.round((change - denominationLabel) * 100) / 100;
+    }
+  }
+
+  //to convert it to array
+  let resultChangeArray = Object.entries(result.change);
+  let resultChangeArrayNoZeros = resultChangeArray
+    .filter((val) => val[1])
+    .sort((a, b) => b[1] - a[1]);
+  result.change = resultChangeArrayNoZeros;
+
+  //to check if the exact cash can be given, even when there is enough cash
+  let totalCashSpent = result.change.reduce((a, val) => a + val[1], 0);
+  return totalCashSpent >= change
+    ? result
+    : { status: "INSUFFICIENT_FUNDS", change: [] };
+}
+
+checkCashRegister(3.26, 100, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
